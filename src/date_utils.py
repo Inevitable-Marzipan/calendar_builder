0bead5b6dccc35048a54d302328b9b2f42e0e721
TimJ <timj@elastacloud.com>
2021-05-19 12:40:07 +0100

    Adds weekday determining function

--- src/date_utils.py
+++ src/date_utils.py
@@ -3,3 +3,26 @@ def is_leap_year(year):
     return ((year % 4) == 0) and \
             (((year % 100) != 0) or \
               (year % 400 == 0))
+
+
+def day_of_week(year, month, day):
+    """
+    Follows ISO 8601 in that 
+    Sunday = 0
+    Monday = 1
+    ...
+    """
+
+    if is_leap_year(year):
+        offsets = [0, 3, 4, 0, 2, 5, 0, 3, 6, 1, 4, 6]
+    else:
+        offsets = [0, 3, 3, 6, 1, 4, 6, 2, 5, 0, 3, 5]
+    months = [x for x in range(1, 13)]
+    month_offset = dict(zip(months, offsets))
+    
+    m = month_offset[month]
+    return (day + 
+            m + 
+            5 * ((year - 1) % 4) +
+            4 * ((year -1) % 100) +
+            6 * ((year -1) % 400)) % 7
--- tests/test_date_utils.py
+++ tests/test_date_utils.py
@@ -1,13 +1,22 @@
 import pytest
 
 from src.date_utils import is_leap_year
+from src.date_utils import day_of_week
 
 
 @pytest.mark.parametrize("year, is_leap", [(1700, False), (1800, False), (1600, True), (2000, True)])
 def test_is_leap_year(year, is_leap):
 
-
     leap_year = is_leap_year(year)
 
     assert leap_year == is_leap
+
+@pytest.mark.parametrize("year, month, day, actual_weekday", [(2021, 5, 19, 3), (2000, 1, 1, 6), (1777, 4, 30, 3)])
+def test_day_of_week(year, month, day, actual_weekday):
+
+    weekday = day_of_week(year, month, day)
+
+    assert weekday == actual_weekday
+
+
 
