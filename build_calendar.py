0036fa8133d20cc8593052081c17d8877ad015a5
TimJ <timj@elastacloud.com>
2021-05-20 11:19:13 +0100

    Adds main builder script

---
+++ build_calendar.py
@@ -0,0 +1,22 @@
+import dominate
+from dominate.tags import *
+
+from src.date_utils import create_month_block
+from src.html_utils import create_html_month_block
+
+
+if __name__ == "__main__":
+
+    year = 2021
+    
+    doc = dominate.document(title=f'Year {year} calendar')
+
+    with doc:
+        for month in range(1, 13):
+            month_block = create_month_block(year, month)
+            html_month_block = create_html_month_block(month, month_block)
+            doc.add(html_month_block)
+
+    output_file = "test.html"
+    with open(output_file, "w") as f:
+        f.write(str(doc))
