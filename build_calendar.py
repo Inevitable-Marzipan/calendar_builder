60c10e60876e92f6ed6e1e8212104f0da01cad9d
TimJ <timj@elastacloud.com>
2021-05-20 16:03:26 +0100

    Adds years and styling

--- build_calendar.py
+++ build_calendar.py
@@ -7,19 +7,21 @@ from src.html_utils import create_html_month_block
 
 if __name__ == "__main__":
 
-    year = 2021
+    base_year = 1995
     
-    doc = dominate.document(title=f'Year {year} calendar')
+    doc = dominate.document(title=f'Year {base_year} +-1 calendar')
 
     with doc.head:
         link(rel='stylesheet', href='style.css')
 
     with doc:
-        with div(cls='grid') as year_div:
-            for month in range(1, 13):
-                month_block = create_month_block(year, month)
-                html_month_block = create_html_month_block(month, month_block)
-                year_div.add(html_month_block)
+        for year in [base_year -1, base_year, base_year + 1]:
+            h1(f'{year}', cls='year')
+            with div(cls='grid') as year_div:
+                for month in range(1, 13):
+                    month_block = create_month_block(year, month)
+                    html_month_block = create_html_month_block(month, month_block)
+                    year_div.add(html_month_block)
 
     output_file = "test.html"
     with open(output_file, "w") as f:
