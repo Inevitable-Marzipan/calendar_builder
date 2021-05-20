17b5d0893c6c792b89088b9c27c959c564e88180
TimJ <timj@elastacloud.com>
2021-05-20 14:21:02 +0100

    Adds grid layout

--- build_calendar.py
+++ build_calendar.py
@@ -15,10 +15,11 @@ if __name__ == "__main__":
         link(rel='stylesheet', href='style.css')
 
     with doc:
-        for month in range(1, 13):
-            month_block = create_month_block(year, month)
-            html_month_block = create_html_month_block(month, month_block)
-            doc.add(html_month_block)
+        with div(cls='grid') as year_div:
+            for month in range(1, 13):
+                month_block = create_month_block(year, month)
+                html_month_block = create_html_month_block(month, month_block)
+                year_div.add(html_month_block)
 
     output_file = "test.html"
     with open(output_file, "w") as f:
